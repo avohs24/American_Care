c4604436dbb10b5209708c9cff69b48481802bcf
Alex Vohs <avohs24@gmail.com>
2017-08-22 23:02:26 -0400

    adding files for heroku deployment

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,9 @@ $(document).ready(function(){
 window.sr = ScrollReveal();
 sr.reveal('.how-it-works', { duration: 1000 });
 sr.reveal('#contact', { duration: 1000 });
+
+
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
